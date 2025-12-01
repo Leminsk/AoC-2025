ef04025261259acb0b30ff44e80277ea70bd378f
Leminsk <lemosreis.matheus@gmail.com>
2025-12-01 15:56:29 +0000

    Day 01 - Part One

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 MAIN_SOURCE = day01_0.cpp
 
 all : $(MAIN_SOURCE)
-	g++ $(MAIN_SOURCE) -lmingw32 -o main
+	g++ $(MAIN_SOURCE) -LC:/msys64/mingw64/lib -lmingw32 -o main
--- day01_0.cpp
+++ day01_0.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <fstream>
+
+int calculateRotation(const int& current_position, const char& direction, const std::string& amount) {
+    int new_position = current_position;
+    int d = std::stoi(amount)%100; // multiples of 100 are a full rotation
+    if(direction == 'R') {
+        new_position += d;
+        if(new_position >= 100) { new_position -= 100; }
+    } else {
+        new_position -= d;
+        if(new_position < 0) { new_position += 100; }
+    }
+    return new_position;
+}
 
 int main() {
-    std::cout << "Hi, mom\n";
+    std::ifstream infile("day01-input.txt");
+    std::string line;
+    std::string num_str;
+    int current_pos = 50;
+    int zero_counter = 0;
+    int line_number = 0;
+    while(std::getline(infile, line)) {
+        ++line_number;
+        if(line.size() == 2) {
+            num_str = line[1];
+        } else {
+            num_str = line.substr(1, line.size()-1);
+        }
+        current_pos = calculateRotation(current_pos, line[0], num_str);
+        if(current_pos == 0) { ++zero_counter; }
+    }
+    std::cout << "zero_counter:" << zero_counter << '\n';
     return 0;
 }
